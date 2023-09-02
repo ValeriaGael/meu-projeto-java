4214030160309895f14e388587e86bd90e545b8e
Valéria Santos <valeriaguilherme.s@gmail.com>
2023-09-01 23:15:49 -0300

    alteração

--- java-anatomia-de-classe/MinhClasse.java
+++ java-anatomia-de-classe/MinhClasse.java
@@ -1,9 +1,17 @@
 public class MinhClasse {
 
     public static void main(String[] args) {
-        
-        System.out.print ("Olá, Mundo!");
 
+        String primeiroNome = "Valéria";
+        String segundoNome = "Santos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+        
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
     
 }
